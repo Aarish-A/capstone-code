7a3731e46c0671ed23c1d26ada5872e50c43867e
Aarish Adeel <aarish8@gmail.com>
2024-04-01 23:22:38 -0400

    Fix build error with string concatenation

--- src/debug.h
+++ src/debug.h
@@ -7,12 +7,12 @@
 #include "bluetooth.h"
 
 template <typename Func, typename... Args>
-unsigned long timeFunctionExecution(const char* functionName, Func func, Args&&... args) {
+unsigned long timeFunctionExecution(std::string functionName, Func func, Args&&... args) {
     unsigned long startTime = micros();
     func(std::forward<Args>(args)...);  // Execute the function with its arguments
     unsigned long endTime = micros();
 
     unsigned long executionTime = (micros() - startTime);
-    logBluetooth(functionName + " Execution time (micros)", executionTime);
+    logBluetooth((functionName + " Execution time (micros)").c_str(), executionTime);
     return executionTime;  // Return execution time in microseconds
 }
